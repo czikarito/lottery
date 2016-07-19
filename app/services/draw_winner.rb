6bcca26a02d31b07f5729e486107e6024d3f53ff
Robert <harezlak.robert7@gmail.com>
2016-07-19 15:47:37 +0200

    spec for rand_winner and send_mail

--- app/services/draw_winner.rb
+++ app/services/draw_winner.rb
@@ -1,7 +1,16 @@
 class DrawWinner < Struct.new(:item)
-
   def can_run_draw?
-    item.bids.count >=2 ? true : false
+    item.bids.count >= 2 ? true : false
   end
 
+  def rand_winner
+    if can_run_draw?
+      winner = item.bids.order('RANDOM()').first
+      item.user_id = winner.user_id
+    end
+  end
+
+  def send_mail
+    UserMailer.send_win_confirmation(item).deliver_now
+  end
 end
--- spec/services/draw_winner_spec.rb
+++ spec/services/draw_winner_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'DrawWinner' do
-  describe '#can_run_draw' do
+  describe '#can_run_draw?' do
     let(:item) { create(:item) }
     let(:draw_winner) { DrawWinner.new(item) }
     context 'yes' do
@@ -13,12 +13,26 @@ RSpec.describe 'DrawWinner' do
       it { expect(draw_winner.can_run_draw?).to eql(true) }
     end
     context 'no' do
-
       it { expect(draw_winner.can_run_draw?).to eql(false) }
     end
   end
 
-  pending '#set_winner'
+  describe '#rand_winner' do
+    let(:item) { create(:item) }
+    let(:draw_winner) { DrawWinner.new(item) }
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let!(:bid1) { item.bids.create(user: user1) }
+    let!(:bid2) { item.bids.create(user: user2) }
+    before { draw_winner.rand_winner }
 
-  pending '#send_win_confirmation'
+    it { expect([user1, user2]).to include(item.user) }
+  end
+
+  describe '#send_mail' do
+    let(:draw_winner) { DrawWinner.new(item) }
+    let(:user) { create(:user) }
+    let(:item) { create(:item, user: user) }
+    it { expect{draw_winner.send_mail}.to change { ActionMailer::Base.deliveries.count}.by(1) }
+  end
 end
