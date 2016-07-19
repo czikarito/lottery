41ff5b68583eeac296dc9806c2ad1e770e9c89e3
Robert <harezlak.robert7@gmail.com>
2016-07-19 12:19:58 +0200

    Refactored controller using before action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   respond_to :html, :json
+  before_action :authenticate_admin, only: [:destroy, :create, :update]
+  before_action :count_bids, only: [:destroy]
 
   expose :q, -> { Item.ransack(search_params) }
   expose :items, -> { q.result(distinct: true).where(user_id: nil).page params[:page] }
@@ -16,40 +18,34 @@ class ItemsController < ApplicationController
   end
 
   def create
-    if current_user && current_user.has_role?(:admin)
-      item.save
-      respond_with(item)
-    elsif current_user
-      redirect_to items_path, notice: 'You cannot create item!'
-    else
-      redirect_to new_user_session_path
-    end
+    item.save
+    respond_with(item)
   end
 
   def update
-    if current_user && current_user.has_role?(:admin)
-      if item.update(item_params)
-        redirect_to item_path(item), notice: 'Item was successfully updated.'
-      end
-    elsif current_user
-      redirect_to items_path, notice: 'You cannot update item!'
-    else
-      redirect_to new_user_session_path
-      end
+    if item.update(item_params)
+      redirect_to item_path(item), notice: 'Item was successfully updated.'
+    end
   end
 
   def destroy
-    if current_user && current_user.has_role?(:admin) && item.bids.count == 0
       item.destroy
       respond_with(item)
-    elsif current_user
-      redirect_to item_path(item), notice: 'You cannot delete item!'
-    else
-      redirect_to new_user_session_path
-    end
-end
+  end
 
   private
+
+  def authenticate_admin
+    unless current_user && current_user.has_role?(:admin)
+      redirect_to new_user_session_path
+    end
+  end
+
+  def count_bids
+    unless item.bids.count == 0
+      redirect_to item_path item
+    end
+  end
 
   def search_params
     params.permit(q: [:name_cont])['q'].to_h
@@ -57,5 +53,5 @@ end
 
   def item_params
     params.require(:item).permit(:name, :description, :image)
-    end
+  end
 end
