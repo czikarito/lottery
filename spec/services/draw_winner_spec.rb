cbe14aff0e1787b15b2660724e48ea889d63fbd6
Robert <harezlak.robert7@gmail.com>
2016-07-19 14:43:03 +0200

    spec for can_run_draw? context no

--- spec/services/draw_winner_spec.rb
+++ spec/services/draw_winner_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 RSpec.describe 'DrawWinner' do
   describe '#can_run_draw' do
     let(:item) { create(:item) }
+    let(:draw_winner) { DrawWinner.new(item) }
     context 'yes' do
       let(:user1) { create(:user) }
       let(:user2) { create(:user) }
-      let!(:bid1) { item.bids.create(user: user1)}
-      let!(:bid2) { item.bids.create(user: user2)}
-      let(:draw_winner) { DrawWinner.new(item) }
+      let!(:bid1) { item.bids.create(user: user1) }
+      let!(:bid2) { item.bids.create(user: user2) }
+
       it { expect(draw_winner.can_run_draw?).to eql(true) }
     end
-    pending 'no'
+    context 'no' do
+
+      it { expect(draw_winner.can_run_draw?).to eql(false) }
+    end
   end
 
   pending '#set_winner'
